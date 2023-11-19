229b84f7ad2928bd3ee1b0f98b15b138da06543f
Harsh Agrawal <113363316+harshagr24@users.noreply.github.com>
2023-11-19 07:27:34 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <p> good morning</p>
+    <ul>
+      <li> mango</li>
+      <li> apple</li>
+      <li> orange </li>
+    </ul>
+    <ol>
+      <li> hi</li>
+      <li> bye</li>
+    </ol>
+  </div>,
+  document.getElementById("root")
+);
